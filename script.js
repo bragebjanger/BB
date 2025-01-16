aaa52c574fcc28a0bb8ca01438f0a2759c95d7f7
bragebjanger <brage.bjanger@gmail.com>
2025-01-16 12:42:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,44 @@
-import express from 'express'
+import express from 'express';
 import HTTP_CODES from './utils/httpCodes.mjs';
 
 const server = express();
-const port = (process.env.PORT || 8000);
+const port = process.env.PORT || 8000;
 
 server.set('port', port);
 server.use(express.static('public'));
+
 
 function getRoot(req, res, next) {
     res.status(HTTP_CODES.SUCCESS.OK).send('Hello World').end();
 }
 
+
+function getPoem(req, res, next) {
+    const poem = `
+        I wrote some code, 
+        it broke in fright,
+        I fixed one bug -
+        now ten take flight.
+    `;
+    res.status(HTTP_CODES.SUCCESS.OK).send(poem).end();
+}
+
+function getQuote(req, res, next) {
+    const quotes = [
+        "The only limit to our realization of tomorrow is our doubts of today. - Franklin D. Roosevelt",
+        "You shall not pass. - Gandalf",
+        "Success is not the key to happiness. Happiness is the key to success. - Albert Schweitzer",
+        "I don't like sand. It's coarse and rough and irritating and it gets everywhere. - Anakin Skywalker",
+        "What you get by achieving your goals is not as important as what you become by achieving your goals. - Zig Ziglar"
+    ];
+    const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+    res.status(HTTP_CODES.SUCCESS.OK).send(randomQuote).end();
+}
+
 server.get("/", getRoot);
+server.get("/tmp/poem", getPoem);
+server.get("/tmp/quote", getQuote);
 
 server.listen(server.get('port'), function () {
-    console.log('server running', server.get('port'));
+    console.log('Server running on port', server.get('port'));
 });
