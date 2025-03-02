8187d39252a7afa5e354ab80cab8b0021f202ee1
bragebjanger <brage.bjanger@gmail.com>
2025-03-02 13:53:28 +0100

    Satt inn hovedstader for testing

--- server.js
+++ server.js
@@ -4,3 +4,10 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
+const capitals = [
+    { country: "Norway", capital: "Oslo" },
+    { country: "Sweden", capital: "Stockholm" },
+    { country: "France", capital: "Paris" },
+    { country: "Japan", capital: "Tokyo" },
+    { country: "Brazil", capital: "Brasilia" }
+];
