7d0f451825b653f68d491611e15c77b88e07024f
bragebjanger <brage.bjanger@gmail.com>
2025-03-12 14:38:25 +0100

    lagt inn dotenv

--- server.cjs
+++ server.cjs
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const pool = require('./db.cjs');
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 app.use(cors());
