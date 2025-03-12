ef0905ec5a9e34c7f3449a276075f48f44fc56ab
bragebjanger <brage.bjanger@gmail.com>
2025-03-12 14:58:52 +0100

    Oppdatert URL

--- server.cjs
+++ server.cjs
@@ -2,8 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const pool = require('./db.cjs');
-const dotenv = require("dotenv");
-dotenv.config();
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
